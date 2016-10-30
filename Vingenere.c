207af6f44259a474011ab48b051ba5e41eb0e0c3
Alex Shults <diaselfurion@iMac-Dias.local>
2016-10-30 21:45:50 +0200

    Normal(lol) code

--- Vingenere.c
+++ Vingenere.c
@@ -1,45 +1,50 @@
-#include <cs50.h> 
+#include <cs50.h>
 #include <stdio.h>
-#include <ctype.h> 
+#include <ctype.h>
 #include <string.h>
-//Check vingenere
-int main(int argc, string argv[])
-{
-if (argc != 2)
-{
-printf("Try again.");
-return 1;
+
+//function that used when something worng
+void TryAgain() {
+	printf("Try Again.\n");
 }
+// validate vingenere key
+int main(int argc, string argv[]){
+if (argc != 2)
+  {
+    TryAgain();
+    return 1;
+  }
 
 string key = argv[1];
 unsigned long keyCount = size(key);
-
-for(int k = 0; k < keyCount; k++) 
-{
-if (!isalpha(key[k]))
-{
-printf("Try again.");
-return 1;
+// validate key only includes alpha characters
+for(int k = 0; k < keyCount; k++)
+  {
+    if (!isalpha(key[k]))
+    {
+      TryAgain();
+      return 1;
+    }
 }
-}
-
+//get string(captain?)
 string text = GetString();
-if(text == NULL) 
+if(text == NULL)
 {
-printf("Try again.");
-return 1;
+  TryAgain();
+  return 1;
 }
-
-for (int i = 0, n = 0; i < size(text); i++) 
+// encrypt and print characters
+for (int i = 0, n = 0; i < size(text); i++)
 {
-if (isalpha(text[i])) 
-{
-if (isupper(text[i]))
-printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount]))-'A')%26) % 26) + 'A'));
+  if (isalpha(text[i]))
+  {
+    if (isupper(text[i]))
+    printf("%c", ((((text[i] - 'A') + ((toupper(key[n++%keyCount]))-'A')%26) % 26) + 'A'));
 
-if (islower(text[i]))
-printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount]))-'a')%26) % 26) + 'a'));
-} else
+    if (islower(text[i]))
+    printf("%c", ((((text[i] - 'a') + ((tolower(key[n++%keyCount]))-'a')%26) % 26) + 'a'));
+  }
+else
 printf("%c", text[i]);
 }
 
